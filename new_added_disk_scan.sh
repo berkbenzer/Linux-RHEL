705ebb653003fa2b6f758d249514fa6e3ad229af
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-09-09 09:21:07 +0300

    Create new_added_disk_scan.sh

---
+++ new_added_disk_scan.sh
@@ -0,0 +1,4 @@
+echo "- - -" > /sys/class/scsi_host/host#/scan
+  193  echo "- - -" > /sys/class/scsi_host/host0/scan 
+  194  echo "- - -" > /sys/class/scsi_host/host1/scan 
+  195  echo "- - -" > /sys/class/scsi_host/host2/scan
