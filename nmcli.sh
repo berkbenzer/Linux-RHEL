6dc45751c7168cf0fe6e07bf628c7aee6081eed6
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-09-23 16:41:49 +0300

    Update nmcli.sh

--- nmcli.sh
+++ nmcli.sh
@@ -1,2 +1,2 @@
  nmcli con del ens192
-	    nmcli con add con-name ens192 ifname ens192 type ethernet ip4 172.17.242.220/24 gw4 172.17.242.110
+	    nmcli con add con-name ens192 ifname ens192 type ethernet ip4 xxx.xxx.xx.xxx/24 gw4 xxx.xxx.xx.xxx
