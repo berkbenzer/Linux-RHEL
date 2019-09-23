d6ad24a5a898c149d400904345c1ca98b9a37be5
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2019-09-23 14:48:48 +0300

    Update SYN_floding.sh

--- IP_TABLES/SYN_floding.sh
+++ IP_TABLES/SYN_floding.sh
@@ -1,1 +1,15 @@
 
+#We can use the limit module of iptables firewall to protect us from SYN flooding.
+#We specify 10 SYN packets per second
+
+iptables -A INPUT -i ens192 -p tcp --syn -m limit --limit 10/second -j ACCEPT
+
+#SYN COOKIES
+
+
+
+#add following line to sysctl.conf
+vi /etc/sysctl.conf
+net.ipv4.tcp_syncookies = 1
+#Then save and reload
+sysctl -p
