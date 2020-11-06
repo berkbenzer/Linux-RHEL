84b7b56845994c3d8eaf7ba7649a590a8cce4095
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2020-11-06 17:51:07 +0300

    Update history_date.sh

--- history_date.sh
+++ history_date.sh
@@ -3,3 +3,5 @@ HISTTIMEFORMAT="%d/%m/%y %T "
 echo 'HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bashrc
 source ~/.bashrc
 source ~/.bash_profile
+
+history -d $((HISTCMD-1)) && history -d [line entry number]
