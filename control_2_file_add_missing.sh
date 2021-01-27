8a85f8ad4a2764ec94843f6eea30bffdd009a219
berkbenzer <52455783+berkbenzer@users.noreply.github.com>
2021-01-27 18:43:49 +0300

    Update control_2_file_add_missing.sh

--- control_2_file_add_missing.sh
+++ control_2_file_add_missing.sh
@@ -2,10 +2,10 @@
 
 
 for i in `cat /home/odeonuser/file1.txt`; do
-      grep -i "$i" /home/odeonuser/file2.txt >/dev/null;
+      grep -i "$i" /home/xxx/file2.txt >/dev/null;
       if [[ $? == 0 ]]; then echo "$i exsists in both files";
         else
           echo "$i doesnt exsists in file2";
-          echo $i >> /home/odeonuser/file2.txt;
+          echo $i >> /home/xxxx/file2.txt;
       fi;
   done
