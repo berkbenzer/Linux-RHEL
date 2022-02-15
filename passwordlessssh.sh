47678436deeb81d4907af118720032a583cd40c3
Berk Benzer <52455783+berkbenzer@users.noreply.github.com>
2022-02-15 14:10:16 +0300

    Update passwordlessssh.sh

--- passwordlessssh.sh
+++ passwordlessssh.sh
@@ -10,17 +10,7 @@ Your public key has been saved in /root/.ssh/id_rsa.pub.
 The key fingerprint is:
 SHA256:CIJwPEOs/UH5eHh74CZQpggAeEigXjbU9nd81kx6JdA root@rheltest
 The key's randomart image is:
-+---[RSA 2048]----+
-|@*o...      .o   |
-|*oB =o        E..|
-|+=.@o+.   .   =..|
-|+ *.=.=o . o + + |
-| . o =.oS . o .  |
-|    o + .        |
-|     o .         |
-|                 |
-|                 |
-+----[SHA256]-----+
+
 [root@rheltest:~]# ssh-copy-id bbenzer@satellite
 /usr/bin/ssh-copy-id: INFO: Source of key(s) to be installed: "/root/.ssh/id_rsa.pub"
 /usr/bin/ssh-copy-id: INFO: attempting to log in with the new key(s), to filter out any that are already installed
@@ -28,7 +18,7 @@ The key's randomart image is:
 
 bbenzer@satellite's password:
 Number of key(s) added: 1
-Now try logging into the machine, with:   "ssh 'bbenzer@satellite'"
+Now try logging into the machine, with:   "ssh 'xxxx@satellite'"
 and check to make sure that only the key(s) you wanted were added.
 [root@rheltest:~]# scp /tmp/ad/
 krb5.conf    limits.conf  smb.conf     sssd.conf    
